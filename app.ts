783da668d1f69ac922d5f906d3cf74cb0474cb6e
Dambi Stuart <stuartdambi@gmail.com>
2021-10-18 12:36:09 +0300

    [add] create product, users, catergory tables with associations

--- app.ts
+++ app.ts
@@ -1,7 +1,18 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
 
-const app = express()
+import db from "./models";
+import userRoutes from "./routes/UserRoutes";
 
-const PORT = process.env.PORT || 5000
+const app = express();
 
-app.listen(PORT, () => console.log(`listening to port ${PORT}`))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1/users", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => console.log(`listening to port ${PORT}`));
+});
