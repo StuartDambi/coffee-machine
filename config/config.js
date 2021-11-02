918276af6f027777e1293f4f1344efe728f33a0e
stuartdambi@gmail.com <stuartdambi@gmail.com>
2021-11-02 15:18:19 +0300

    [add] delpoyment to heroku config

--- config/config.js
+++ config/config.js
@@ -19,11 +19,6 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
+    "use_env_variable": "DATABASE_URL",
   },
 };
