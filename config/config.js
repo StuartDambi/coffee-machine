31b8abec314e8682fbaa01e719702f928fdb11da
stuartdambi@gmail.com <stuartdambi@gmail.com>
2021-11-11 13:35:55 +0300

    [fix] enable ssl for database url

--- config/config.js
+++ config/config.js
@@ -19,7 +19,7 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    "use_env_variable": "DATABASE_URL",
+    "use_env_variable": process.env.DATABASE_URL,
     dialectOptions: {
       ssl: {
         require: true,
